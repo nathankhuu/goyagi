369413d86be42e6557aa2e8177f5910f1de48d69
nathankhuu <nathan.khuu@lob.com>
2019-08-20 16:50:05 -0700

    feat(movies): add list and retrieve movies endpoints

--- cmd/serve/main.go
+++ cmd/serve/main.go
@@ -1,23 +1,29 @@
 package main
 
 import (
-    "net/http"
+	"net/http"
 
-    "github.com/nathankhuu/goyagi/pkg/server"
-    "github.com/lob/logger-go"
+	"github.com/lob/logger-go"
+	"github.com/nathankhuu/goyagi/pkg/application"
+	"github.com/nathankhuu/goyagi/pkg/server"
 )
 
 func main() {
-    log := logger.New()
+	log := logger.New()
 
-    srv := server.New()
+	app, err := application.New()
+	if err != nil {
+		log.Err(err).Fatal("failed to initialize application")
+	}
 
-    log.Info("server started")
+	srv := server.New(app)
 
-    err := srv.ListenAndServe()
-    if err != nil && err != http.ErrServerClosed {
-        log.Err(err).Fatal("server stopped")
-    }
+	log.Info("server started", logger.Data{"port": app.Config.Port})
 
-    log.Info("server stopped")
+	err = srv.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		log.Err(err).Fatal("server stopped")
+	}
+
+	log.Info("server stopped")
 }
